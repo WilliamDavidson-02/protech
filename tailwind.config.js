c2f90ab07fca0d33cf8b99ac4e4847df51ed5150
William Davidson <wj.davidson@outlook.com>
2023-11-07 21:13:10 +0100

    initial files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html", "./*.js"],
+  theme: {
+    extend: {
+      colors: {
+        "pro-black": "#1D1D1F",
+        "pro-gray": "#86868B",
+        "pro-white": "#F5F5F7",
+        "pro-gold": "#D9B573",
+        "pro-light-purple": "#A352D9",
+        "pro-dark-purple": "#7937A6",
+      },
+    },
+  },
+  plugins: [],
+};
