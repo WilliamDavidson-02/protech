ec892b6c7fe47803e7bf7cf1b5f367aa85fab826
William Davidson <wj.davidson@outlook.com>
2023-11-08 15:40:53 +0100

    aside toggle

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 const menuBtn = document.querySelector("#menu-btn");
 const menuLines = document.querySelectorAll("#menu-btn #menu-line");
 
-menuBtn.addEventListener("click", () => {
+const aSideContainer = document.querySelector("#aside-container");
+const aSideInner = document.querySelector("#aside-inner");
+
+const toggleMenuBar = () => {
   const [firstLine, secondLine, thirdLine] = menuLines;
+  if (!aSideContainer.classList.contains("hidden")) {
+    setTimeout(() => {
+      aSideContainer.classList.toggle("hidden");
+    }, 300);
+  } else {
+    aSideContainer.classList.toggle("hidden");
+  }
+  setTimeout(() => {
+    aSideInner.classList.toggle("translate-x-0");
+  }, 10);
+
   if (secondLine.classList.contains("w-0")) {
     secondLine.classList.remove("w-0", "opacity-0");
+    secondLine.classList.add("w-8");
     firstLine.classList.remove(
       "-rotate-45",
       "w-[calc-(19*1.41421356237)px]",
@@ -17,6 +32,7 @@ menuBtn.addEventListener("click", () => {
     );
   } else {
     secondLine.classList.add("w-0", "opacity-0");
+    secondLine.classList.remove("w-8");
     firstLine.classList.add(
       "-rotate-45",
       "w-[calc-(19*1.41421356237)px]",
@@ -28,4 +44,6 @@ menuBtn.addEventListener("click", () => {
       "translate-y-[3px]"
     );
   }
-});
+};
+
+menuBtn.addEventListener("click", toggleMenuBar);
