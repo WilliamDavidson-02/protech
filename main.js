c3d650bb9bf130428f5b7d535a32a7242a0c237b
William Davidson <wj.davidson@outlook.com>
2023-11-09 21:18:32 +0100

    mobile nalinks toggle, email form

--- main.js
+++ main.js
@@ -6,6 +6,28 @@ const aSideInner = document.querySelector("#aside-inner");
 
 const dropdownBtns = document.querySelectorAll("#dropdown-btn");
 const expandIcons = document.querySelectorAll("#expand-icon");
+let prevDropdownIndex = null;
+
+const forms = document.querySelectorAll("#form");
+
+const toggleDropdown = (index) => {
+  expandIcons[index].textContent =
+    expandIcons[index].textContent.trim() === "expand_more"
+      ? "expand_less"
+      : "expand_more";
+
+  const childToExpand =
+    dropdownBtns[index].children[dropdownBtns[index].children.length - 1];
+
+  childToExpand.addEventListener("click", (ev) => {
+    ev.stopPropagation();
+  });
+
+  childToExpand.style.height =
+    childToExpand.clientHeight === 0 ? `${childToExpand.scrollHeight}px` : 0;
+
+  childToExpand.classList.toggle("my-4");
+};
 
 const toggleMenuBar = () => {
   const [firstLine, secondLine, thirdLine] = menuLines;
@@ -24,6 +46,7 @@ const toggleMenuBar = () => {
   secondLine.classList.toggle("opacity-0");
 
   if (secondLine.classList.contains("w-0")) {
+    // collapse
     secondLine.classList.replace("w-0", "w-8");
     firstLine.classList.remove(
       "-rotate-45",
@@ -35,7 +58,12 @@ const toggleMenuBar = () => {
       "w-[calc-(19*1.41421356237)px]",
       "translate-y-[3px]"
     );
+    if (prevDropdownIndex !== null) {
+      toggleDropdown(prevDropdownIndex);
+      prevDropdownIndex = null;
+    }
   } else {
+    // open
     secondLine.classList.replace("w-8", "w-0");
     firstLine.classList.add(
       "-rotate-45",
@@ -59,15 +87,33 @@ window.addEventListener("resize", () => {
 
 dropdownBtns.forEach((btn, index) => {
   btn.addEventListener("click", () => {
-    expandIcons[index].textContent =
-      expandIcons[index].textContent.trim() === "expand_more"
-        ? "expand_less"
-        : "expand_more";
-    const childToExpand = btn.children[btn.children.length - 1];
-    childToExpand.addEventListener("click", (ev) => {
-      ev.stopPropagation();
-    });
-    childToExpand.style.height =
-      childToExpand.clientHeight === 0 ? `${childToExpand.scrollHeight}px` : 0;
+    if (prevDropdownIndex !== index && prevDropdownIndex !== null) {
+      toggleDropdown(prevDropdownIndex);
+    }
+    toggleDropdown(index);
+    prevDropdownIndex = prevDropdownIndex !== index ? index : null;
+  });
+});
+
+forms.forEach((form) => {
+  form.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+
+    const email = form.querySelector("#email");
+    const emailIcon = form.querySelector("#emailIcon");
+    const emailRegxp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (emailRegxp.test(email.value)) {
+      emailIcon.classList.add("text-green-500");
+      setTimeout(() => {
+        emailIcon.classList.remove("text-green-500");
+      }, 2000);
+      email.value = "";
+    } else {
+      emailIcon.classList.add("text-red-500", "animate-shake");
+      setTimeout(() => {
+        emailIcon.classList.remove("text-red-500", "animate-shake");
+      }, 500);
+    }
   });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ export default {
   content: ["./*.html", "./*.js"],
   theme: {
     extend: {
+      keyframes: {
+        shake: {
+          "0%, 100%": { transform: "translateX(0)" },
+          "10%, 30%, 50%, 70%, 90%": { transform: "translateX(-2px)" },
+          "20%, 40%, 60%, 80%": { transform: "translateX(2px)" },
+        },
+      },
+      animation: {
+        shake: "shake 0.5s ease-in-out forwards",
+      },
       colors: {
         "pro-black": "#1D1D1F",
         "pro-gray": "#86868B",
