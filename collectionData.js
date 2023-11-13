2fba08361504d3cbc6b7394ba1f34e7b318888df
William Davidson <wj.davidson@outlook.com>
2023-11-13 15:20:21 +0100

    moved images to public folder

--- collectionData.js
+++ collectionData.js
@@ -1,24 +1,24 @@
 const collectionData = [
   [
     {
-      img: "./assets/classic_ring.png",
+      img: "public/classic_ring.png",
       title: "Klassisk Ring",
       description: "Klassisk ring.",
     },
     {
-      img: "assets/Custom Classic Gold.png",
+      img: "public/Custom Classic Gold.png",
       title: "Klassiskt Armband",
       description: "Klassisk armband.",
     },
   ],
   [
     {
-      img: "./assets/Ring Tech.png",
+      img: "public/Ring Tech.png",
       title: "Tech Ring",
       description: "Senaste ringen med de senaste funktionerna.",
     },
     {
-      img: "assets/Ring Tech 2.png",
+      img: "public/Ring Tech 2.png",
       title: "Tech Armband",
       description: "Senaste ringen med de senaste funktionerna.",
     },
