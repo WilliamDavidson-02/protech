ff5673280b6cd14e3e0e307913098ae38eb00688
William Davidson <wj.davidson@outlook.com>
2023-11-17 16:09:03 +0100

    3d key chain rotation on mouse move

--- three.js
+++ three.js
@@ -1,7 +1,11 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { RectAreaLightUniformsLib } from "three/addons/lights/RectAreaLightUniformsLib.js";
+
+// Convert degrees to radians
+const toRadians = (degrees) => {
+  return degrees * (Math.PI / 180);
+};
 
 const toComeCanvas = document.querySelector("#to-come-canvas");
 
@@ -14,16 +18,12 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.x = -3;
-camera.rotation.y = 180;
-
-const ambLight = new THREE.AmbientLight(0xfdf5e8, 1);
-// scene.add(ambLight);
+camera.position.set(0, 0, 3.3);
 
 RectAreaLightUniformsLib.init();
 
 const rectLight1 = new THREE.RectAreaLight(0xfdf5e8, 6, 5, 5);
-rectLight1.position.set(-3, 2, -1);
+rectLight1.position.set(-3, 2, 2);
 rectLight1.lookAt(0, 0, 0);
 scene.add(rectLight1);
 
@@ -36,15 +36,12 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(toComeCanvas.clientWidth, toComeCanvas.clientHeight);
 toComeCanvas.appendChild(renderer.domElement);
 
+let keyChain;
+
 const loader = new GLTFLoader();
 loader.load("/Keychain.gltf", (gltf) => {
-  const keyChain = gltf.scene.children[0];
+  keyChain = gltf.scene.children[0];
   keyChain.scale.set(0.7, 0.7, 0.7);
-
-  keyChain.rotation.set(0, 0, 0);
-  keyChain.rotation.z = THREE.MathUtils.degToRad(90);
-  keyChain.rotation.y = THREE.MathUtils.degToRad(-90);
-  keyChain.rotation.x = THREE.MathUtils.degToRad(90);
 
   keyChain.position.y = -1;
   scene.add(gltf.scene);
@@ -52,19 +49,59 @@ loader.load("/Keychain.gltf", (gltf) => {
   animate();
 });
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.autoRotate = true;
-controls.autoRotateSpeed = 2;
-controls.enablePan = false;
-controls.enableZoom = false;
-controls.maxPolarAngle = THREE.MathUtils.degToRad(90);
-controls.minPolarAngle = 0;
+let isMouseDown = false;
+let prevDirectionY = 0;
+let prevDirectionX = 0;
+
+const onMouseDown = (ev) => {
+  ev.preventDefault();
+  isMouseDown = true;
+};
+const onMouseUp = (ev) => {
+  ev.preventDefault();
+  isMouseDown = false;
+  prevDirectionX = 0;
+  prevDirectionY = 0;
+};
+const onMouseMove = (ev) => {
+  if (!isMouseDown) return;
+  ev.preventDefault();
+
+  const damping = 0.03;
+  const speed = 0.5;
+
+  if (prevDirectionX < ev.clientY) {
+    keyChain.rotation.x += speed * damping;
+  } else {
+    keyChain.rotation.x -= speed * damping;
+  }
+
+  if (prevDirectionY < ev.clientX) {
+    keyChain.rotation.y += speed * damping;
+  } else {
+    keyChain.rotation.y -= speed * damping;
+  }
+
+  prevDirectionY = ev.clientX;
+  prevDirectionX = ev.clientY;
+};
 
 function animate() {
   requestAnimationFrame(animate);
 
-  controls.update();
+  if (!isMouseDown) {
+    if (Math.abs(keyChain.rotation.y) > toRadians(0.1)) {
+      keyChain.rotation.y -= keyChain.rotation.y * 0.03;
+    } else {
+      keyChain.rotation.y = 0;
+    }
+
+    if (Math.abs(keyChain.rotation.x) > toRadians(0.1)) {
+      keyChain.rotation.x -= keyChain.rotation.x * 0.03;
+    } else {
+      keyChain.rotation.x = 0;
+    }
+  }
 
   renderer.render(scene, camera);
 }
@@ -74,3 +111,7 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
   renderer.setSize(toComeCanvas.clientWidth, toComeCanvas.clientHeight);
 });
+
+toComeCanvas.addEventListener("mousedown", onMouseDown);
+window.addEventListener("mouseup", onMouseUp);
+window.addEventListener("mousemove", onMouseMove);
