78be8ba87125ed9c3c4e7cdd239dcf02d14e5ccc
William Davidson <wj.davidson@outlook.com>
2023-11-17 11:36:36 +0100

    3d keychain placment and lighting

--- three.js
+++ three.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { RectAreaLightUniformsLib } from "three/addons/lights/RectAreaLightUniformsLib.js";
 
 const toComeCanvas = document.querySelector("#to-come-canvas");
 
@@ -8,30 +9,28 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color("#F4EAD7");
 
 const camera = new THREE.PerspectiveCamera(
-  75,
+  65,
   toComeCanvas.clientWidth / toComeCanvas.clientHeight,
   0.1,
   1000
 );
-camera.position.x = 5;
+camera.position.x = -3;
+camera.rotation.y = 180;
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 15);
-directionalLight.position.set(0, 2, 0);
-directionalLight.castShadow = true;
-scene.add(directionalLight);
+const ambLight = new THREE.AmbientLight(0xfdf5e8, 1);
+// scene.add(ambLight);
 
-const directionalLightBack = new THREE.DirectionalLight(0xffffff, 3);
-directionalLightBack.position.set(-2, 2, 0);
-directionalLightBack.castShadow = true;
-scene.add(directionalLightBack);
+RectAreaLightUniformsLib.init();
 
-const pointLight1 = new THREE.PointLight(0xc4c4c4, 20);
-pointLight1.position.set(2, 0, -5);
-scene.add(pointLight1);
+const rectLight1 = new THREE.RectAreaLight(0xfdf5e8, 6, 5, 5);
+rectLight1.position.set(-3, 2, -1);
+rectLight1.lookAt(0, 0, 0);
+scene.add(rectLight1);
 
-const pointLight2 = new THREE.PointLight(0xc4c4c4, 20);
-pointLight2.position.set(3, -2, 3);
-scene.add(pointLight2);
+const rectLight2 = new THREE.RectAreaLight(0xfdf5e8, 5, 5, 5);
+rectLight2.position.set(3, 2, 1);
+rectLight2.lookAt(0, 0, 0);
+scene.add(rectLight2);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(toComeCanvas.clientWidth, toComeCanvas.clientHeight);
@@ -40,23 +39,26 @@ toComeCanvas.appendChild(renderer.domElement);
 const loader = new GLTFLoader();
 loader.load("/Keychain.gltf", (gltf) => {
   const keyChain = gltf.scene.children[0];
-  keyChain.scale.set(0.8, 0.8, 0.8);
-  keyChain.position.x = 1.5;
-  keyChain.position.y = -1.8;
+  keyChain.scale.set(0.7, 0.7, 0.7);
 
-  keyChain.rotation.x = 90;
+  keyChain.rotation.set(0, 0, 0);
+  keyChain.rotation.z = THREE.MathUtils.degToRad(90);
+  keyChain.rotation.y = THREE.MathUtils.degToRad(-90);
+  keyChain.rotation.x = THREE.MathUtils.degToRad(90);
 
+  keyChain.position.y = -1;
   scene.add(gltf.scene);
+
   animate();
 });
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.autoRotate = true;
-controls.autoRotateSpeed = 7;
+controls.autoRotateSpeed = 2;
 controls.enablePan = false;
 controls.enableZoom = false;
-controls.maxPolarAngle = Math.PI / 2;
+controls.maxPolarAngle = THREE.MathUtils.degToRad(90);
 controls.minPolarAngle = 0;
 
 function animate() {
